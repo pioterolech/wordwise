52645a03f39474d791bbb0be67300e581c8c4aef
Piotr Olechnowicz <olechnowicz.p@gmail.com>
2023-08-03 13:28:03 +0300

    Add wordwise app

---
+++ WordWiseApp/WordWise/WordWise/WordWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordWiseApp.swift
+//  WordWise
+//
+//  Created by Piotr Olechnowicz on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WordWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
